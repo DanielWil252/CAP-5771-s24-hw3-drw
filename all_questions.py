af64384476765e48bd39e14518cb7f3116d093d3
DanielWil252 <dan.rei.wilcox@gmail.com>
2024-03-04 22:38:41 -0500

    potential fix for q5

--- all_questions.py
+++ all_questions.py
@@ -161,13 +161,12 @@ def question5():
     answers = {}
 
     # type: set
-    answers["(a)"] = set(['A','B'])
-
+    answers["(a)"] = set(['AB'])
     # type: explanatory string (at least four words)
     answers["(a) explain"] = "For MIN heirarchical clustering, the clusters with the shortest distance between their closest points would be considered first. In this case, it would be groups A and B."
 
     # type: set
-    answers["(b)"] = set(['A','C'])
+    answers["(b)"] = set(['AC'])
 
     # type: explanatory string (at least four words)
     answers["(b) explain"] = "For MAX heirarchical clustering, the clusters with the shortest maximum distance from their members would be considered for merging. In this case, it would be groups A and C."
